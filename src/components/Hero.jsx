44edd646bb82ab39c75254e4665d8138771b25b5
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-14 22:03:00 +0600

    changed image using useRef

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
+import { useRef } from "react";
 
 const Hero = () => {
+
+    let myImage = useRef();
+    const change = ()=>{
+        myImage.src = "https://c4.wallpaperflare.com/wallpaper/316/497/192/kung-fu-panda-kung-fu-panda-3-po-kung-fu-panda-wallpaper-preview.jpg"
+    }
     return (
         <div>
             <img src="https://miro.medium.com/v2/resize:fit:1200/1*wLCHEekWiQAj-Q-Fg_8zcg.jpeg" alt="Kung Fu Panda"
@@ -9,7 +15,13 @@ const Hero = () => {
                     height: 'auto',
                     marginLeft : 'auto',
                 }
-             } />
+             } ref={(img)=> myImage = img} />
+
+             <button onClick={change} style={
+                {
+                    marginLeft : "20px",
+                }
+             }>Click</button>
         </div>
     );
 };
