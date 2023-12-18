77da790d799800012994e509da820e743e8838b5
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-18 22:53:03 +0600

    installed milligram-css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "milligram"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
