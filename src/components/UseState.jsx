d843f67ef49a27e608c1d3d2089abe33c6b0937d
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-18 20:48:20 +0600

    updating immutable object replacing new one with spread operator for react re-rendering issue

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 
 const UseState = () => {
-    const [number, setNumber] = useState(null);
+    const [number, setNumber] = useState(10);
     const change = () => {
         setNumber(number + 10);
     }
+
+    const [myObject, setMyObject] = useState({
+        key1: 'value1',
+        key2: 'value2',
+        key3: 'value3',
+    });
+    const changeObj = () => {
+        setMyObject(myObject => ({
+            ...myObject,
+            key1: 'new value 1',
+            key2: 'new value 2'
+        }));
+    };
+
     return (
         <div>
+            <h1>{myObject.key2}</h1>
+            <button onClick={changeObj} >Change Object </button>
             <h1>Number : {number}</h1>
-            <button onClick={change} >Click</button>
+            <button onClick={change} >Change Number</button>
         </div>
     );
 };
