f8c4672da6a9aa8443e212743bc021ea56cfeaa1
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-20 23:33:21 +0600

    changed color using useRef and current

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -3,7 +3,11 @@ import { useEffect, useState, useRef } from "react";
 const UseEffect = () => {
 
     const [Data, SetData] = useState([]);
+    const headline = useRef();
 
+    const change = () => {
+        headline.current.style.color = 'blue';
+    }
     useEffect(() => {
         (
             async () => {
@@ -17,6 +21,8 @@ const UseEffect = () => {
     return (
         <div>
             {JSON.stringify(Data)}
+            <h1 ref={headline}>I have to do hard work for Learning</h1>
+            <button onClick={change}>Click</button>
         </div>
     );
 };
