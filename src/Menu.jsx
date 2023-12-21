b283f8b41fadc745c85f5725fd061bbd73aed365
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-21 08:28:18 +0600

    used NavLink and showed response in color change if link is active or pending

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './assets/css/style.css'
 
 const Menu = () => {
     return (
         <div>
             <div>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/effect">UseEffect</Link></li>
-                    <li><Link to="/todo">Todo</Link></li>
-                    <li><Link to="/form">Form</Link></li>
-                    <li><Link to="/demo">Demo</Link></li>
-                    <li><Link to="/contactForm">Contact Form</Link></li>
-                    <li><Link to="/random">Random</Link></li>
-                    <li><Link to="*">Not Found</Link></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/">Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/effect">UseEffect</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/todo">Todo</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/form">Form</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/demo">Demo</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/contactForm">Contact Form</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="/random">Random</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "active-item" : "pending-item"} to="*">Not Found</NavLink></li>
                 </ul>
             </div>
         </div>
