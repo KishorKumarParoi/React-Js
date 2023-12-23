b6a0acc9c085bd985d056c020e2d491480aee130
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-24 00:48:55 +0600

    applied dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js}"],
+  // purge: [],
+  darkMode: "media",
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
