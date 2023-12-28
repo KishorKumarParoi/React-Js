cdcf7a6bdf0dd7ca74c89bf4f6da26e3aee16194
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-28 23:33:25 +0600

    learned variant ordering, theme extended, screen resizing, changing default font etc from documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import {defaultTheme} from "tailwindcss";
+
 export default {
   content: ["./src/**/*.{html,js}"],
   mode: "jit",
@@ -7,6 +10,14 @@ export default {
   ],
   darkMode: "media",
   theme: {
+    screens: {
+      sm : '540px',
+      lg : '1000px',
+      "4k" : "2560px"
+    },
+    fontFamily: {
+      sans : ["Roboto", ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   variants: {
