7fd74680c80e3274c334e5fb5f3e6b010c18edfb
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-12-29 11:52:44 +0600

    learned extend variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
-import {defaultTheme} from "tailwindcss";
+// import {defaultTheme} from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{html,js}"],
   mode: "jit",
-  purge: [
-    "./src/*.{html,js}"
-  ],
   darkMode: "media",
   theme: {
-    screens: {
-      sm : '540px',
-      lg : '1000px',
-      "4k" : "2560px"
-    },
     fontFamily: {
-      sans : ["Roboto", ...defaultTheme.fontFamily.sans]
+      'sans': ["Roboto"]
     },
-    extend: {},
+    extend: {
+      screens: {
+        'ssm': '540px',
+        'llg': '1000px',
+        'mmd': '780px',
+        "4k": "2560px"
+      }
+    },
   },
   variants: {
     extend: {},
