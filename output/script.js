5339df4dbeba91b9c9f43450c48c4c981b22ce43
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-01 21:54:31 +0600

    type alias learned

---
+++ output/script.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("Hello KKP!");
+var country = "I love Bangladesh";
+console.log(country);
+var kishor = function () {
+    console.log("Hello Async Function");
+};
+var userDetails = function (id, user) {
+    console.log("User id is ".concat(id, ", User name is ").concat(user.name, " and age is ").concat(user.age));
+};
+var sayHello = function (user) {
+    console.log("Hello (".concat(user.age, " > 50 ? \"Sir\" : \"Mr.) ").concat(user.name));
+};
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,23 @@
-console.log('Hello KKP!');
+console.log("Hello KKP!");
 const country = "I love Bangladesh";
 console.log(country);
 
-const kishor = ()=> {
-    console.log('Hello Async Function');
-}
+const kishor = () => {
+  console.log("Hello Async Function");
+};
+
+type stringOrNumber = string | number;
+type userType = {
+  name: string;
+  age: number;
+};
+
+const userDetails = (id: stringOrNumber, user: userType) => {
+  console.log(
+    `User id is ${id}, User name is ${user.name} and age is ${user.age}`
+  );
+};
+
+const sayHello = (user: userType) => {
+  console.log(`Hello (${user.age} > 50 ? "Sir" : "Mr.) ${user.name}`);
+};
