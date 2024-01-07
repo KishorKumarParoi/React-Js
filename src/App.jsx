bf2820cfe3b8d4bf30c74a188c29c5dfe55c15fd
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-07 19:30:08 +0600

    fixed all jsx links

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import Demo from "./components/Demo";
+import Demo from "./components/Demo.jsx";
 import Form from "./components/Form.jsx";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
 import Todo from "./components/Todo.jsx";
 import Menu from "./Menu.jsx";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import UseEffect from './components/useEffect';
-import NotFound from './components/NotFound';
+import UseEffect from './components/UseEffect.jsx';
+import NotFound from './components/NotFound.jsx';
 import Random from "./components/Random.jsx";
 import TailwindCSS from "./components/TailwindCSS.jsx";
-import Button from "./components/button.jsx";
+import Button from "./components/Button.jsx";
 import ContactForm from "./components/ContactForm.jsx";
 import Footer from './components/Footer.jsx';
 
